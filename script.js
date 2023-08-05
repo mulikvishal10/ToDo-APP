d5e46c38190d4298f860fa4c771dcf79198b9a06
vishal <mulikvishal10@gmail.com>
2023-08-05 14:34:23 +0530

    user added

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
 function addTodo() {
-  const todoInput = document.getElementById("todoInput");
-  const todoText = todoInput.value.trim();
-
-  if (todoText !== "") {
-    const todoList = document.getElementById("todoList");
-    const li = document.createElement("li");
-    li.className = "todo-item";
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.onchange = toggleTodoStatus;
-
-    const todoTextElement = document.createElement("span");
-    todoTextElement.textContent = todoText;
-
-    li.appendChild(checkbox);
-    li.appendChild(todoTextElement);
-    todoList.appendChild(li);
-
-    todoInput.value = "";
-  }
+  // ... existing addTodo() function remains unchanged ...
 }
 
 function toggleTodoStatus(event) {
-  const todoTextElement = event.target.nextElementSibling;
-  if (event.target.checked) {
-    todoTextElement.style.textDecoration = "line-through";
-  } else {
-    todoTextElement.style.textDecoration = "none";
+  // ... existing toggleTodoStatus() function remains unchanged ...
+}
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function assignTask() {
+  const usersSelect = document.getElementById("usersSelect");
+  const selectedUser = usersSelect.value;
+  const todoList = document.getElementById("todoList");
+  const todos = todoList.getElementsByClassName("todo-item");
+  let assignedTask = null;
+
+  // Find the first unassigned task
+  for (let i = 0; i < todos.length; i++) {
+    const todo = todos[i];
+    if (!todo.dataset.assigned) {
+      assignedTask = todo;
+      break;
+    }
+  }
+
+  if (assignedTask !== null) {
+    assignedTask.dataset.assigned = "true";
+
+    const assignmentsList = document.getElementById("assignmentsList");
+    const li = document.createElement("li");
+    li.textContent = `${assignedTask.getElementsByTagName("span")[0].textContent} [${selectedUser}]`;
+    assignmentsList.appendChild(li);
   }
 }
